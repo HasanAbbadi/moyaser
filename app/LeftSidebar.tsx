3cb9936699b6e297d0134b8c6ee4a78e8fe47531
HasanAbbadi <hasanabady55@gmail.com>
2023-03-11 09:42:54 +0200

    save theme preference fix

--- app/LeftSidebar.tsx
+++ app/LeftSidebar.tsx
@@ -6,28 +6,30 @@ export default function LeftSidebar({ colorschemes }: any) {
   const [theme, setTheme] = useState('nord-light')
 
   useEffect(() => {
-    localStorage.setItem('theme', theme)
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('theme', theme)
+    }
   }, [theme])
 
-    return (
-        <form className="color-picker" action="">
-          <fieldset>
-            <legend className="visually-hidden">Pick a color scheme</legend>
-            {colorschemes.map((cs: string, i: number) => {
-              {
-                return (
-                  <div className="colorscheme-container" key={i}>
-                    <label htmlFor={cs} className="visually-hidden">{cs}</label>
-                    <input defaultChecked={localStorage.getItem('theme') === cs} type="radio" name="theme" id={cs}
-                     onChange={(e:any) => {setTheme(e.target.id)}} />
-                  </div>
-                )
+  return (
+    <form className="color-picker" action="">
+      <fieldset>
+        <legend className="visually-hidden">Pick a color scheme</legend>
+        {colorschemes.map((cs: string, i: number) => {
+          {
+            return (
+              <div className="colorscheme-container" key={i}>
+                <label htmlFor={cs} className="visually-hidden">{cs}</label>
+                <input defaultChecked={localStorage.getItem('theme') === cs} type="radio" name="theme" id={cs}
+                  onChange={(e: any) => { setTheme(e.target.id) }} />
+              </div>
+            )
 
-              }
-            })}
-          </fieldset>
-        </form>
+          }
+        })}
+      </fieldset>
+    </form>
 
-    )
+  )
 
 }
